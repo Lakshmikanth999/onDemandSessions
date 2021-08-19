fb2efde2b7206539763e739f878d1615151e5e74
Lakshmikanth999 <69025648+Lakshmikanth999@users.noreply.github.com>
2021-08-19 18:56:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 import './index.css'
 
-const Header = () => (
-  <nav className="nav-header">
-    <div className="nav-content">
-      <p className="website-logo">RA</p>
-      <ul className="nav-menu">
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-linkedin-img.png"
-            alt="Linked In"
-          />
-        </li>
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-github-img.png"
-            alt="Git Hub"
-          />
-        </li>
-        <li>
-          <img
-            className="social-network-img"
-            src="https://assets.ccbp.in/frontend/react-js/projects-twitter-img.png"
-            alt="Twitter"
-          />
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+const TabItem = props => {
+  const {tabDetails, clickTabItem, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
 
-export default Header
+  const activeTabBtnClassName = isActive ? 'active-tab-btn' : ''
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabBtnClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
