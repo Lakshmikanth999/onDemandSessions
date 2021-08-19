0339cd2ff63ce1550a691fdf8eab58b63caacc3e
Lakshmikanth999 <69025648+Lakshmikanth999@users.noreply.github.com>
2021-08-19 18:57:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const TabItem = props => {
-  const {tabDetails, clickTabItem, isActive} = props
-  const {tabId, displayText} = tabDetails
-  const onClickTabItem = () => {
-    clickTabItem(tabId)
-  }
-
-  const activeTabBtnClassName = isActive ? 'active-tab-btn' : ''
-
-  return (
-    <li className="tab-item-container ">
-      <button
-        type="button"
-        className={`tab-btn ${activeTabBtnClassName}`}
-        onClick={onClickTabItem}
-      >
-        {displayText}
-      </button>
-    </li>
-  )
-}
-
-export default TabItem
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
